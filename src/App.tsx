bbb1e38b26f49bae013fd40537d7e3e177778522
Ddiaalv <ddiaalv@gmail.com>
2022-01-01 10:30:05 +0000

    :art: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className="App-logo">DDIAALV</p>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
